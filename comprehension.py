af35dd47a8a6227bfe460b8b6f72a5b2896ec482
Anita <irsevica@users.noreply.github.com>
2018-05-27 13:42:20 +1200

    add comprehension examples

---
+++ comprehension.py
@@ -0,0 +1,8 @@
+m = [[1,2,3],[4,5,6],[7,8,9]]
+col2 = [row[2] for row in m]
+print(col2)
+#  adds 1 to each filtered column
+colplus = [row[1] + 1 for row in m ]
+print(colplus)
+# filters out odd nums
+colif = [row[1] for row in m if row[1] % 2 == 0]
