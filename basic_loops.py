381f2bf66a2341d51763356addb6b9e415bd295a
irsevica <anita.irsevic@gmail.com>
2018-10-01 12:06:59 +1300

    add additional iteration examples

--- basic_loops.py
+++ basic_loops.py
@@ -7,3 +7,40 @@ x=0
 for c in s: x +=ord(c)
 
 print(x)
+
+ordlist = []
+for c in s:
+  ordlist.append(ord(c))
+
+print(ordlist)
+
+ordlist = map(ord, s)
+print(ordlist)
+ordist = [ord(c) for c in s]
+print(ordlist)
+
+L = [1, 2, 4, 8, 16, 32, 64]
+X = 5
+i = 0
+while i < len(L):
+  if 2 ** X == L[i]:
+    print('at index', i)
+    break
+  else:
+    i = i+1
+
+for i in L:
+  sum = 2 ** X
+  res = L.index(sum)
+  if res:
+    print('at index', res )
+    break
+
+res = [L.index(item) for item in L if 2 ** X == item]
+print('at index', res)
+
+for item in L:
+  x = 2 ** item
+  L.append(x)
+
+print(L)
